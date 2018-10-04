a8f3b3aec104eae275ae747a238d0b85a9fa648c
jifeihu <jiflytiger@163.com>
2018-10-04 16:42:29 +0800

    update sql

--- smartmonitor.sql
+++ smartmonitor.sql
@@ -1,26 +1,229 @@
-/*
- Navicat Premium Data Transfer
+-- MySQL dump 10.13  Distrib 5.7.23, for macos10.13 (x86_64)
+--
+-- Host: 127.0.0.1    Database: smartmonitor
+-- ------------------------------------------------------
+-- Server version	5.7.23
 
- Source Server         : localhost
- Source Server Type    : MySQL
- Source Server Version : 50723
- Source Host           : localhost:3306
- Source Schema         : smartmonitor
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
- Target Server Type    : MySQL
- Target Server Version : 50723
- File Encoding         : 65001
+--
+-- Table structure for table `tb_area`
+--
 
- Date: 30/09/2017 17:25:51
-*/
+DROP TABLE IF EXISTS `tb_area`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `tb_area` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) DEFAULT NULL,
+  `description` text,
+  `column_4` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
-SET NAMES utf8mb4;
-SET FOREIGN_KEY_CHECKS = 0;
+--
+-- Dumping data for table `tb_area`
+--
 
--- ----------------------------
--- Table structure for tb_user
--- ----------------------------
+LOCK TABLES `tb_area` WRITE;
+/*!40000 ALTER TABLE `tb_area` DISABLE KEYS */;
+/*!40000 ALTER TABLE `tb_area` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `tb_dataType`
+--
+
+DROP TABLE IF EXISTS `tb_dataType`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `tb_dataType` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) DEFAULT NULL,
+  `description` text,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `tb_dataType`
+--
+
+LOCK TABLES `tb_dataType` WRITE;
+/*!40000 ALTER TABLE `tb_dataType` DISABLE KEYS */;
+/*!40000 ALTER TABLE `tb_dataType` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `tb_device`
+--
+
+DROP TABLE IF EXISTS `tb_device`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `tb_device` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) DEFAULT NULL,
+  `state` bit(1) DEFAULT NULL,
+  `node_name` varchar(255) DEFAULT NULL,
+  `area_name` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `tb_device`
+--
+
+LOCK TABLES `tb_device` WRITE;
+/*!40000 ALTER TABLE `tb_device` DISABLE KEYS */;
+/*!40000 ALTER TABLE `tb_device` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `tb_log`
+--
+
+DROP TABLE IF EXISTS `tb_log`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `tb_log` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `type` int(11) DEFAULT NULL,
+  `time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `content` text,
+  `username` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `tb_log`
+--
+
+LOCK TABLES `tb_log` WRITE;
+/*!40000 ALTER TABLE `tb_log` DISABLE KEYS */;
+/*!40000 ALTER TABLE `tb_log` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `tb_node`
+--
+
+DROP TABLE IF EXISTS `tb_node`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `tb_node` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) DEFAULT NULL,
+  `description` text,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `tb_node`
+--
+
+LOCK TABLES `tb_node` WRITE;
+/*!40000 ALTER TABLE `tb_node` DISABLE KEYS */;
+/*!40000 ALTER TABLE `tb_node` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `tb_sensor`
+--
+
+DROP TABLE IF EXISTS `tb_sensor`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `tb_sensor` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) DEFAULT NULL,
+  `unit` varchar(20) DEFAULT NULL,
+  `real_value` int(11) DEFAULT NULL,
+  `device_id` int(11) DEFAULT NULL,
+  `area_name` varchar(255) DEFAULT NULL,
+  `node_name` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `tb_sensor`
+--
+
+LOCK TABLES `tb_sensor` WRITE;
+/*!40000 ALTER TABLE `tb_sensor` DISABLE KEYS */;
+/*!40000 ALTER TABLE `tb_sensor` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `tb_sensor_value`
+--
+
+DROP TABLE IF EXISTS `tb_sensor_value`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `tb_sensor_value` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `sensor_id` int(11) DEFAULT NULL,
+  `time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `tb_sensor_value`
+--
+
+LOCK TABLES `tb_sensor_value` WRITE;
+/*!40000 ALTER TABLE `tb_sensor_value` DISABLE KEYS */;
+/*!40000 ALTER TABLE `tb_sensor_value` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `tb_threshold`
+--
+
+DROP TABLE IF EXISTS `tb_threshold`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `tb_threshold` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `max` int(11) DEFAULT NULL,
+  `min` int(11) DEFAULT NULL,
+  `sensor_id` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `tb_threshold`
+--
+
+LOCK TABLES `tb_threshold` WRITE;
+/*!40000 ALTER TABLE `tb_threshold` DISABLE KEYS */;
+/*!40000 ALTER TABLE `tb_threshold` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `tb_user`
+--
+
 DROP TABLE IF EXISTS `tb_user`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `tb_user` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `username` varchar(100) DEFAULT NULL,
@@ -31,12 +234,25 @@ CREATE TABLE `tb_user` (
   `tel` varchar(11) DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
--- ----------------------------
--- Records of tb_user
--- ----------------------------
-BEGIN;
-INSERT INTO `tb_user` VALUES (1, 'feihu', 1, 'e10adc3949ba59abbe56e057f20f883e', 'xiaobai', 1, '13094029161');
-COMMIT;
+--
+-- Dumping data for table `tb_user`
+--
 
-SET FOREIGN_KEY_CHECKS = 1;
+LOCK TABLES `tb_user` WRITE;
+/*!40000 ALTER TABLE `tb_user` DISABLE KEYS */;
+INSERT INTO `tb_user` VALUES (1,'feihu',1,'e10adc3949ba59abbe56e057f20f883e','xiaobai',1,'13094029161');
+/*!40000 ALTER TABLE `tb_user` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2018-10-04 16:41:37
